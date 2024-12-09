d023c4eb9c9d15d2717fb0d49e42ac926b3a8e98
kooqooo <koogmlcks@gmail.com>
2024-12-10 00:32:56 +0900

    fix: 윈도우 대응 + 중첩된 리스트 제거
    #8

--- data/db_manager.py
+++ data/db_manager.py
@@ -6,15 +6,25 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 dimension = 1536 if os.getenv("EMBEDDING_MODEL").endswith("small") else 3072
 db_path = os.path.join(os.path.dirname(__file__), "milvus.db")
+
+
 client = pymilvus.MilvusClient(db_path)
+if os.name == 'nt':
+    client = pymilvus.MilvusClient() # docker-compose가 실행중이어야 가능합니다.
+
 
 def create_vector_index(collection_name: str="faq", data: list[dict]=None):
     if data is None:
         raise ValueError("data must be provided")
-
     client.create_collection(collection_name=collection_name, dimension=dimension)
     client.insert(collection_name=collection_name, data=data)
 
-if __name__ == "__main__":
-    create_vector_index(data=[])
 
+if __name__ == "__main__":
+    from preprocess_pickle import load_pickle
+    
+    embedded_pickle_path = os.path.join(os.path.dirname(__file__), "embedded_data.pkl")
+    data = load_pickle(embedded_pickle_path)
+    
+    data = [{"vector": d["vector"][0], **{k:v for k,v in d.items() if k != "vector"}} for d in data if isinstance(d["vector"][0], list)]
+    create_vector_index(data=data)
