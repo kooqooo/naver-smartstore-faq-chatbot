6f5a697ce0f31c1f8f79dd216b96177af1b1d641
kooqooo <koogmlcks@gmail.com>
2024-12-09 21:12:07 +0900

    feat: 텍스트 임베딩 후 벡터로 반환하는 함수 구현
    #7

--- src/vectorstore/embeddings.py
+++
@@ -1,9 +0,0 @@
-from src.config import EMBEDDING_MODEL, client as openai_client
-
-if __name__ == "__main__":
-    dimensions = 1536 if EMBEDDING_MODEL.endswith('small') else 3072
-    result = openai_client.embeddings.create(input='hello world', model=EMBEDDING_MODEL, dimensions=dimensions)
-
-    data = result.data[0]
-    embedding = data.embedding
-    dimensions = len(embedding)
