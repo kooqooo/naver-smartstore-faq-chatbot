66aee6c03d5e84428a3c26c9b6f2a6a7a9f55c63
kooqooo <koogmlcks@gmail.com>
2024-12-10 21:32:23 +0900

    feat: streamlit으로 예시 테스트
    #11

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from src.front.utils import delete_session_state, get_response_stream, write_message
+from src.front.utils import (delete_session_state,
+                            get_response_stream,
+                            write_message,
+                            write_messages,
+                            add_messages_to_session_state,)
+                            # add_backend_messages_to_session_state,)
 from src.message_template import Message, Messages
+from data.db_manager import client as milvus_client
 
 # 페이지 설정
 st.set_page_config(page_title="스마트스토어 FAQ 챗봇", layout="centered")
@@ -18,21 +24,30 @@ if "chat_started" not in st.session_state:  # 채팅이 시작되었는지 여�
     st.session_state.chat_started = False
 if "messages" not in st.session_state:  # 화면에 표시할 메시지
     st.session_state.messages = Messages()
+if "backend_messages" not in st.session_state:  # 백엔드에 전달할 메시지
+    st.session_state.backend_messages = Messages()
+if "system_prompt_messages" not in st.session_state:  # 프롬프트를 담은 메시지
+    chat_system_prompt_path = "prompts/chat_system_prompt.txt"
+    st.session_state.system_prompt_messages = Messages.from_prompt_file(chat_system_prompt_path)
+if "milvus" not in st.session_state:  # Milvus 클라이언트
+    st.session_state.milvus = milvus_client
 if "client" not in st.session_state:  # OpenAI 클라이언트
     load_dotenv()
     st.session_state.client = OpenAI()
 
 
 # 채팅 내용 표시
-for message in st.session_state.messages:
-    write_message(message)
+write_messages(st.session_state.messages)
 
 if not st.session_state.chat_started:
-    init_assistant_message = Message(
+    init_assistant_message = Messages()
+    init_assistant_message.add_message(
         role="assistant",
         content="안녕하세요. 네이버 스마트스토어 질의응답 챗봇입니다. 무엇을 도와드릴까요?",
     )
-    write_message(init_assistant_message)
+    write_messages(init_assistant_message)
+    add_messages_to_session_state(init_assistant_message)
+    st.session_state.chat_started = True
 
 # user_input = st.chat_input("텍스트를 입력하세요.")
 if user_input := st.chat_input("텍스트를 입력하세요."):
@@ -49,13 +64,26 @@ if user_input := st.chat_input("텍스트를 입력하세요."):
     ]:
         delete_session_state()
         st.stop()
-    user_message = Message(role="user", content=user_input)
-    st.session_state.messages += user_message
-    write_message(user_message)
+    user_message = Messages()
+    user_message.add_message(role="user", content=user_input) # 구매 확정에 대해서 알려주세요
+    write_messages(user_message)
+    add_messages_to_session_state(user_message)
+
+    # reference 넣어주기
+    reference = """
+    ### question: 구매확정은 무엇인가요?
+    ### answer:
+구매자가 판매자에게 상품 주문 또는 서비스를 제공받은 후 해당 상품을 정상 수령하였거나 서비스에 대해 만족한 경우 "구매확정" 처리를 하게 됩니다.
+이처럼 구매확정 처리는 판매자에게 구매대금을 정산 해도 된다는 구매자의 의사표시이기 때문에 구매확정된 이후 판매자에게 정산이 진행됩니다. 
+
+※ 참고. 구매자가 구매확정을 지연시킬 경우를 대비하여,  일정 기간이 지나면 자동으로 구매확정 처리됩니다. 
+
+자동 구매확정 도움말 바로 가기》"""
+
+    st.session_state.backend_messages = st.session_state.system_prompt_messages.render_all({"reference": reference})
+    st.session_state.backend_messages += st.session_state.messages
 
     # 챗봇 대답
     with st.chat_message("assistant"):
-        st.write_stream(get_response_stream(user_input))
+        st.write_stream(get_response_stream(st.session_state.backend_messages))
 
-    # 채팅 시작
-    st.session_state.chat_started = True
