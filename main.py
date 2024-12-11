c35da11926eae45e82fd4cea4e397ca15ab291e5
kooqooo <koogmlcks@gmail.com>
2024-12-11 18:56:57 +0900

    feat: chat 엔드포인트에서 관련 FAQ 문서 찾는 기능 추가
    [FEAT] `FastAPI`로 간단한 API 만들기 #13

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import List
-from src.config import *
+from src.config import CHAT_MODEL, chat_system_prompt_path, client as chat_client
 from src.message_template import Messages
-
+from data.embeddings import embed_question
+from data.db_manager import search_from_faq
 
 app = FastAPI()
+system_prompt_messages = Messages.from_prompt_file(chat_system_prompt_path)
 
 
 class ChatRequest(BaseModel):
@@ -19,17 +21,22 @@ async def chat_endpoint(request: ChatRequest):
     TODO
     1. (추가 예정) context를 적용한 새로운 적절한 질문 생성
     2. 더 적절한 FAQ 데이터 찾기 (voting, ranking 등 후처리가 필요할 수도?)
-    3. System prompt 추가
-    4. render_all을 사용하여 불러온 데이터로 렌더링
+    3. [완료] System prompt 추가
+    4. [완료] render_all을 사용하여 불러온 데이터로 렌더링
     """
     messages = Messages()
     for message in request.messages:
         messages.add_message(message["role"], message["content"])
+    user_input = messages.messages[-1].content
+    embedded_user_input = embed_question(user_input)
+    reference = search_from_faq(embedded_user_input, limit=5) # threshold를 0.4로 설정
+    messages_with_reference = system_prompt_messages.render_all({"reference": str(reference)})
+    messages_with_reference += messages
 
     async def generate():
-        response = client.chat.completions.create(
+        response = chat_client.chat.completions.create(
             model=CHAT_MODEL,
-            messages=messages.to_dict(),
+            messages=messages_with_reference.to_dict(),
             stream=True
         )
 
